3c56ad05a9641dd48cfbc90f071dc382a5c97e5c
BuiQuangSang-25810039 <culumsieunhan@gmail.com>
2026-09-24 12:39:08 +0700

    Buoi02 Bai06 day so Fibonacci

---
+++ Bai06_Fibonacci.kt
@@ -0,0 +1,18 @@
+// Bui Quang Sang - MSSV: 25810039
+
+fun main() {
+    var firstNumber = 0
+    var secondNumber = 1
+
+    for (position in 0..20) {
+        if (firstNumber >= 100) {
+            break
+        }
+
+        println("Vi tri $position: $firstNumber")
+
+        val nextNumber = firstNumber + secondNumber
+        firstNumber = secondNumber
+        secondNumber = nextNumber
+    }
+}
