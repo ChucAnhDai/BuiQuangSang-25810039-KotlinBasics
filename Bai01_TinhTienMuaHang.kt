dca0ef791b5216aa0f087d44ec587c047d331e87
BuiQuangSang-25810039 <culumsieunhan@gmail.com>
2026-09-24 12:39:08 +0700

    Buoi02 Bai01 tinh tien mua hang

---
+++ Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,12 @@
+// Bui Quang Sang - MSSV: 25810039
+
+fun main() {
+    val quantity: Int = 3
+    val unitPrice: Double = 50_000.0
+
+    val moneyBeforeTax = quantity.toDouble() * unitPrice
+    val tax = moneyBeforeTax * 0.08
+    val totalMoney = moneyBeforeTax + tax
+
+    println("Tong tien phai tra: $totalMoney VND")
+}
