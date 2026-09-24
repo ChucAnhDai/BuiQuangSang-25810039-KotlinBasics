3342f65beaeaee54a8b3b4e7666f812ce9278020
BuiQuangSang-25810039 <culumsieunhan@gmail.com>
2026-09-24 12:39:08 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,10 @@
+// Bui Quang Sang - MSSV: 25810039
+
+fun main() {
+    val fullName = "Bui Quang Sang"
+    val birthYear = 2007
+    val currentYear = 2026
+    val age = currentYear - birthYear
+
+    println("Xin chao, minh la $fullName va nam nay minh $age tuoi.")
+}
