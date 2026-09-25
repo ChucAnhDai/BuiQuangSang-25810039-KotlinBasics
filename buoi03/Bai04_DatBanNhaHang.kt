4cd36dfb549d0c68385570a509ad52452b33ae69
BuiQuangSang-25810039 <culumsieunhan@gmail.com>
2026-09-25 19:51:17 +0700

    Buoi03 Bai04 dat ban nha hang

---
+++ buoi03/Bai04_DatBanNhaHang.kt
@@ -0,0 +1,19 @@
+// Bui Quang Sang - MSSV: 25810039
+
+fun datBan(
+    tenKhachHang: String,
+    soLuongKhach: Int,
+    loaiBan: String = "Ban thuong"
+) {
+    println("$tenKhachHang dat $loaiBan cho $soLuongKhach khach.")
+}
+
+fun main() {
+    datBan("An", 2)
+    datBan("Binh", 4, "Ban VIP")
+    datBan(
+        tenKhachHang = "Chi",
+        soLuongKhach = 6,
+        loaiBan = "Ban gia dinh"
+    )
+}
