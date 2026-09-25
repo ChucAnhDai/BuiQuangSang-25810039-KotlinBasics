30fcbb2d8af912d6b38b418784c2133587db6e80
BuiQuangSang-25810039 <culumsieunhan@gmail.com>
2026-09-25 19:51:16 +0700

    Buoi03 Bai02 tinh dien tich

---
+++ buoi03/Bai02_TinhDienTich.kt
@@ -0,0 +1,13 @@
+// Bui Quang Sang - MSSV: 25810039
+
+fun tinhDienTich(chieuDai: Double, chieuRong: Double): Double {
+    return chieuDai * chieuRong
+}
+
+val dienTichPhongKhach = tinhDienTich(5.0, 4.0)
+val dienTichPhongNgu = tinhDienTich(3.0, 4.0)
+
+fun main() {
+    println("Dien tich phong khach: $dienTichPhongKhach m2")
+    println("Dien tich phong ngu: $dienTichPhongNgu m2")
+}
